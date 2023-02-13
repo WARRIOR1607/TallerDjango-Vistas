1d555030fee83cd952c602cc9685e1d988f4fc03
Andres guerrero a.guerreros <a.guerreros@uniandes.edu.co>
2023-02-12 23:29:29 -0500

    cambios

--- measurements/logic/measurements_logic.py
+++ measurements/logic/measurements_logic.py
@@ -1,4 +1,6 @@
 from ..models import Measurement
+from variables import models
+from variables.models import Variable
 
 def get_measurements():
     measurements = Measurement.objects.all()
@@ -9,13 +11,18 @@ def get_measurement(meas_pk):
     return measurement
 
 def create_measurement(meas):
-    measurement = Measurement(name=meas["name"])
+    variable_pk = Variable.objects.get(pk=meas["variable"])
+    measurement = Measurement(variable= variables_pk,value=meas["value"],unit=meas["unit"],place=meas["place"],dateTime=meas["dateTime"])
     measurement.save()
     return measurement
 
 def update_measurement(meas_pk, new_meas):
     measurement = get_measurement(meas_pk)
-    measurement.name = new_meas["name"]
+    measurement.value = new_meas["value"]
+    measurement.unit = new_meas["unit"]
+    measurement.place = new_meas["place"]
+    measurement.dateTime = new_meas["dateTime"]
+
     measurement.save()
     return measurement
 
